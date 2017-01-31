b6b0b4de90677f6384940824936f998a7c4890fd
hditx <pablogabrieloporto@gmail.com>
2017-01-31 15:34:06 -0300

    base de datos

--- config/baseDatos.sql
+++ config/baseDatos.sql
@@ -27,13 +27,13 @@ CREATE TABLE `Cliente` (
   `telefono` int(13) DEFAULT NULL,
   `obraSocial` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`idCliente`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
 /*Data for the table `Cliente` */
 
 LOCK TABLES `Cliente` WRITE;
 
-insert  into `Cliente`(`idCliente`,`nombreApellido`,`dni`,`direccion`,`telefono`,`obraSocial`) values (1,'',111,'',0,''),(2,'',3333,'',0,''),(3,'',5555,'',0,'');
+insert  into `Cliente`(`idCliente`,`nombreApellido`,`dni`,`direccion`,`telefono`,`obraSocial`) values (1,NULL,3,NULL,NULL,NULL),(2,'',1,'',0,''),(3,'',0,'',0,''),(4,'',0,'',0,''),(5,'',313,'',0,''),(6,'',0,'',0,''),(7,'',1234,'',0,''),(8,'',0,'',0,''),(9,'',22,'',0,''),(10,'',0,'',0,''),(11,'',879,'',0,''),(12,'',0,'',0,''),(13,'',23,'',0,''),(14,'',0,'',0,''),(15,'',0,'',0,''),(16,'',0,'',0,''),(17,'',1277777,'',0,''),(18,'',0,'',0,''),(19,'',0,'',0,''),(20,'',0,'',0,'');
 
 UNLOCK TABLES;
 
@@ -45,15 +45,35 @@ CREATE TABLE `Cola` (
   `idCola` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `nombreCola` varchar(30) DEFAULT NULL,
   `idEmpleado` int(10) DEFAULT NULL,
-  `jerarquia` int(10) DEFAULT NULL,
+  `hijoDe` int(10) DEFAULT NULL,
+  `siguiente` int(10) DEFAULT NULL,
+  `letra` char(1) DEFAULT NULL,
   PRIMARY KEY (`idCola`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 /*Data for the table `Cola` */
 
 LOCK TABLES `Cola` WRITE;
 
-insert  into `Cola`(`idCola`,`nombreCola`,`idEmpleado`,`jerarquia`) values (7,'perfumeria',1,0),(8,'farmacia',2,0),(9,'Obra social',0,1),(10,'hnn',2,0);
+insert  into `Cola`(`idCola`,`nombreCola`,`idEmpleado`,`hijoDe`,`siguiente`,`letra`) values (9,'Obra social',0,NULL,NULL,NULL),(11,'Osde',3,9,7,'A'),(12,'Pami',2,9,7,'B'),(13,'Otros',1,NULL,54,'C');
+
+UNLOCK TABLES;
+
+/*Table structure for table `Turno` */
+
+DROP TABLE IF EXISTS `Turno`;
+
+CREATE TABLE `Turno` (
+  `idTurno` int(10) NOT NULL AUTO_INCREMENT,
+  `idCola` int(10) DEFAULT NULL,
+  `idCliente` int(10) DEFAULT NULL,
+  `posicion` int(10) DEFAULT NULL,
+  PRIMARY KEY (`idTurno`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `Turno` */
+
+LOCK TABLES `Turno` WRITE;
 
 UNLOCK TABLES;
 
