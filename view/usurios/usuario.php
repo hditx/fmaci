624cad2327036d3bf1130cf6358fb6a4248358df
Emmanuel Frati <emmanuelfrati@gmail.com>
2017-02-12 20:37:33 -0300

    Actualizo usuario.php para usar la clase myButton en los enlaces.

--- view/usurios/usuario.php
+++ view/usurios/usuario.php
@@ -1,8 +1,8 @@
 <img style="float: left; width: 75px;" src="view/images/logo.png"/><h1>Sistema de gestion de turnos</h1>
 <br />
 <div id="menuPrincipal">
-<a href="index.php?c=cola&a=index">Administrador</a> &nbsp;
-<a href="index.php?c=empleado&a=index">Empleado</a>&nbsp;
-<a href="index.php?c=turno&a=index">Cliente</a>&nbsp;
-<a href="index.php?c=turno&a=monitor">Monitor</a>
+    <a class="myButton" href="index.php?c=cola&a=index">Administrador</a> &nbsp;
+    <a class="myButton" href="index.php?c=empleado&a=index">Empleado</a>&nbsp;
+    <a class="myButton" href="index.php?c=turno&a=index">Cliente</a>&nbsp;
+    <a class="myButton" href="index.php?c=turno&a=monitor">Monitor</a>
 </div>
