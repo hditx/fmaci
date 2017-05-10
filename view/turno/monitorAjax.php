28484be1b979ec59beb82511448454c3bb0a0da8
hditx <hditx@lewits>
2017-05-10 15:52:25 -0300

    jquery

--- view/turno/monitorAjax.php
+++ view/turno/monitorAjax.php
@@ -1,15 +1,25 @@
 <html>
 <head>
-<script type="text/javascript" src="jquery-1.9.0.min.js"></script>
-<script type="text/javascript" src="my_script.js">
-    $(document).ready(function(){
-        function loadRefresh(){
-        $("#refresh").load("view/turno/refresh.php");
-        setInterval(loadRefresh,1000);
-    });}
-</script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="view/stylesheet.css">
+    <title>
+        Farmacentro
+    </title>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+    <script>
+        $(document).on("ready", function(){
+            function reloadTurno(){
+                $.get('view/turno/refresh.php', function(data){
+                    $("#refresh").html(data);
+                    
+                });
+            }
+            setInterval(reloadTurno,1000);
+        });
+    </script>
 </head>
 <body>
-<div id="refresh"></div>
+    <div id="refresh"></div>
 </body>
 </html>
--- view/turno/refresh.php
+++ view/turno/refresh.php
@@ -1,9 +1,17 @@
 <?php
-require_once 'config/DataBase.php';
 require_once 'model/Turno.php';
+require_once 'config/DataBase.php';
 
+$turnos = Turno::getMonitor();
 
-
-    echo "HOLA";
-
+echo "<div id='derecha'>
+        <div class='letra'>Turno</div>";    
+        foreach ($turnos as $t) {
+            if($t->getAtendido() == 2){
+                echo "<div class='modif'>". Turno::getLetra($t->getIdCola())."".$t->getPosicion()."</div>";
+            }else {
+                echo "<div class='letra'>".Turno::getLetra($t->getIdCola())."".$t->getPosicion()."</td>";
+            } 
+        }
+echo "</div>";
 ?>
