82189b197eb228f9cde2011e4770ce0e4cd4cfc3
Emmanuel Frati <emmanuelfrati@gmail.com>
2017-06-05 11:05:20 -0300

    Agrego hojas de estilo awesome!

--- view/header.php
+++ view/header.php
@@ -2,7 +2,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="view/stylesheet.css">
-        <link rel="stylesheet" type="text/css" href="view/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="view/font-awesome-4.7.0/css/font-awesome.min.css">
 
         <title>
             Farmacentro
