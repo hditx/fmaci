051906ef45c5f0e2b24fbe8cdbbdaa27a45a4a83
hditx <hditx@lewits>
2017-06-07 11:39:57 -0300

    Modificacion en llamado de turno reflejado en el monitor

--- view/empleado/llamarTurno.php
+++ view/empleado/llamarTurno.php
@@ -9,7 +9,7 @@
     </div>
 </div>
 <div class="center">
-    <a href="index.php?c=empleado&a=estadoTurno&id=<?= $id?>&estado=2&idEmpleado=<?=$idEmpleado?>"><div class="botonLlama">LLamar nuevamente</div></a>
+    <a href="index.php?c=empleado&a=estadoTurno&id=<?= $id?>&estado=2&idEmpleado=<?= $_SESSION['usuario'] ?>"><div class="botonLlama">LLamar nuevamente</div></a>
     <div class="posicion" id="llamados" >
         <div id="mensaje">
             <?php foreach ($listLlamado as $t) {?>
