98d0e42b471252e5f627a0d9222f7244f9d7141c
hditx <hditx@lewits>
2017-07-06 17:27:01 -0300

    Cambiando la vista del login

--- view/usuario/login.php
+++ view/usuario/login.php
@@ -2,10 +2,10 @@
 <div align="center"><br>
     <form method="POST" action="index.php?c=usuario&a=validateSession" align="center" name="login">
     <div>
-        <img class="userbox" src="view/images/user.ico" height="15px" width="15px" > <input class="inputText" name="usuario" class="user" type="text" placeholder="Usuario">
+        <img class="userbox" src="view/images/user.ico" height="15px" width="15px" > <input  name="usuario" class="user" type="text" placeholder="Usuario">
     </div>
     <div>
-        <img class="userbox"  src="view/images/lock.ico" height="17px" width="17px" > <input class="inputText" name="pass" class="user" type="password" placeholder="Contraseña">
+        <span>  <img class="userbox"  src="view/images/lock.ico" height="30%" width="10%" > <input name="pass" class="user" type="password" placeholder="Contraseña"></span>
     </div>
 	<input type="submit" value="Entrar">
 </form>
