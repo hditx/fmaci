6a92b6890eeaa73a7ea160527d0cf6c07397545b
hditx <pablogabrieloporto@gmail.com>
2017-10-02 12:01:32 -0300

    correccion

--- view/usuario/login.php
+++ view/usuario/login.php
@@ -2,7 +2,7 @@
 <div align="center"><br>
     <form method="POST" action="index.php?c=usuario&a=validateSession" align="center" name="login">
     <div>
-        <img class="userbox" src="view/images/user.ico" height="15px" width="15px" > <input name="usuario" class="user" type="password" placeholder="Usuario" <?php switch ($_REQUEST['monitor']){case 1: echo "value='24'";break; case 2: echo "value='23'";break;default: echo "value=''";break;}?>>
+        <img class="userbox" src="view/images/user.ico" height="15px" width="15px" > <input name="usuario" autofocus class="user" type="password" placeholder="Usuario" <?php switch ($_REQUEST['monitor']){case 1: echo "value='24'";break; case 2: echo "value='23'";break;default: echo "value=''";break;}?>>
     </div><br/>
 	<input type="submit" value="Entrar">
 </form>
