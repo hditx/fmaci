89e25a12ebc680235d4a24f4c4fde37750ca2f7c
hditx <pablogabrieloporto@gmail.com>
2020-06-06 17:26:06 -0300

    Correcion de estilo

--- view/administrador/informes.php
+++ view/administrador/informes.php
@@ -3,6 +3,20 @@
    <div class="card">
        <div class="card-header text-center">
            <h1>Informes</h1>
+           <ul class="nav nav-pills card-header-pills">
+               <li class="nav-item">
+                   <a class="nav-link active" href="#">Turnos Emitidos</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link" href="#">Turnos Atendidos</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link disabled" href="#">Empleados</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link disabled" href="#">Tiempo de Espera</a>
+               </li>
+           </ul>
        </div>
        <div class="card-body">
             <form action="index.php?c=administrador&a=informe" method="post">
